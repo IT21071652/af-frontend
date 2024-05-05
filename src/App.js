631fe9c38923bf26de218a53e7f4c91e716ed98a
IT21071652 <it21071652@my.sliit.lk>
2024-05-05 11:38:22 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
-import { BrowserRouter as Route, Routes } from 'react-router-dom';
+
+import { Route, Routes, Navigate } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import ApodPage from './components/ApodPage';
 import MarsRoverPage from './components/MarsRoverPage';
 import EpicPage from './components/EpicPage';
 
-// import { Navigate } from 'react-router-dom';
+
 	
 
 function App() {
-	//const user = localStorage.getItem("token");
+	
 	
 	return (
 		<Routes>
-				<Route path="/" exact element={<HomePage/>} />
-				<Route path="/Home" exact element={<HomePage/>} />
+				{<Route path="/" exact element={<HomePage/>} />}
+
+				
                 <Route path="/apod" exact element={<ApodPage/>} />
 				<Route path="/mars-rover" exact element={<MarsRoverPage/>} />
                 <Route path="/epic" exact element={<EpicPage/>} />
 				<Route path="/logout" exact element={<HomePage/>} />
-				{/* <Route path="/" element={<Navigate replace to="/Home" />} /> */}
+				<Route path="/" element={<Navigate replace to="/Home" />} />
 
 
 
